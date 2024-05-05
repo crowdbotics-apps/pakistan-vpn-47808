a0741b7c80436cd38be11811d7a4f77d199921b2
Crowdbotics <team@crowdbotics.com>
2024-05-05 19:05:45 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "pakistan-vpn-47808",
+  url: "https://pakistan-vpn-47808.botics.co",
+  api: "https://pakistan-vpn-47808.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
